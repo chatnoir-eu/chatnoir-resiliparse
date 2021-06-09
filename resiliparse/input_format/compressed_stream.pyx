dc22ee1ed49843fc968947a655938b0139a0d608
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-06-09 18:25:05 +0200

    Add native stream reader implementation

--- resiliparse/input_format/compressed_stream.pyx
+++ resiliparse/input_format/compressed_stream.pyx
@@ -1,35 +1,106 @@
 # distutils: language = c++
 
-import io
-import zlib
+from libcpp.string cimport string
 
-cdef int BUFF_SIZE = 16384
+cdef extern from "zlib.h":
+    ctypedef void *gzFile
+    ctypedef size_t z_off_t
 
+    int gzclose(gzFile fp)
+    gzFile gzopen(char* path, char* mode)
+    int gzread(gzFile fp, void* buf, unsigned int n)
+    char* gzerror(gzFile fp, int* errnum)
 
-class GZipStream(io.BufferedReader):
-    def __init__(self, raw, buffer_size=io.DEFAULT_BUFFER_SIZE):
-        super().__init__(raw, buffer_size)
-        self.decomp_obj = None
-        self._unused_data = None
-        self._buf = b''
+cdef size_t BUFF_SIZE = 16384
 
-        self._init_decomp_obj()
+cdef class CompressedStream:
+    cdef void open(self, string file, string mode):
+        pass
 
-    def _init_decomp_obj(self):
-        self._unused_data = b''
-        self.decomp_obj = zlib.decompressobj(16 + zlib.MAX_WBITS)
+    cdef void close(self):
+        pass
 
-    def _fill_buffer(self, size):
-        if not self._unused_data:
-            self._buf = super().read(size)
-        elif self._unused_data:
-            self._buf = self._unused_data
-            self._init_decomp_obj()
-        else:
-            self._buf = b''
+    cdef int errnum(self):
+        return 0
 
-    def read(self, size=BUFF_SIZE):
-        self._fill_buffer(size)
-        decomp = self.decomp_obj.decompress(self._buf)
-        self._unused_data = self.decomp_obj.unused_data
-        return decomp
+    cdef string error(self):
+        pass
+
+    cdef string read(self, size_t size=BUFF_SIZE):
+        pass
+
+cdef class GZipStream(CompressedStream):
+    cdef gzFile fp
+
+    def __init__(self, string filename, string mode):
+        self.fp = NULL
+        self.open(filename, mode)
+
+    def __dealloc__(self):
+        self.close()
+
+    cdef void open(self, string file, string mode):
+        self.fp = gzopen(file.c_str(), mode.c_str())
+
+    cdef void close(self):
+        if self.fp:
+            gzclose(self.fp)
+            self.fp = NULL
+
+    cdef int errnum(self):
+        cdef int errnum
+        gzerror(self.fp, &errnum)
+        return errnum
+
+    cdef string error(self):
+        cdef int errnum
+        return gzerror(self.fp, &errnum)
+
+    cdef string read(self, size_t size=BUFF_SIZE):
+        cdef string buf
+        buf.resize(size)
+        cdef int l = gzread(self.fp, &buf.front(), size)
+        return buf.substr(0, l)
+
+cdef class LineParser:
+    cdef CompressedStream stream
+    cdef string buf
+
+    def __init__(self, CompressedStream stream):
+        self.stream = stream
+        self.buf = string(b'')
+
+    cdef bint _fill_buf(self, size_t buf_size):
+        if self.buf.size() >= buf_size:
+            return True
+
+        cdef string tmp_buf
+        tmp_buf = self.stream.read(buf_size)
+        if tmp_buf.empty():
+            return False
+        self.buf.append(tmp_buf)
+        return True
+
+    cpdef string readline(self, size_t max_line_len=4096, size_t buf_size=BUFF_SIZE):
+        cdef string line
+        cdef size_t pos
+        cdef size_t npos = <size_t>-1
+        cdef string tmp_buf
+
+        if not self._fill_buf(buf_size):
+            return line
+
+        pos = self.buf.find(b'\n')
+        while pos == npos and not self.buf.empty():
+            if line.size() < max_line_len:
+                line.append(self.buf.substr(0u, min(self.buf.size(), max_line_len - line.size())))
+            # Consume rest of line
+            self.buf.clear()
+            if not self._fill_buf(buf_size):
+                break
+            pos = self.buf.find(b'\n')
+
+        line.append(self.buf.substr(0u, min(pos + 1, max_line_len - line.size())))
+        self.buf = self.buf.substr(min(pos + 1, self.buf.size() - 1))
+
+        return line
