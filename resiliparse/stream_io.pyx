352be43e7ab6a6c39a27a5c9fa49eefb24f1b065
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-06-10 10:51:11 +0200

    Extend stream interfaces

--- resiliparse/stream_io.pyx
+++ resiliparse/stream_io.pyx
@@ -14,25 +14,34 @@
 
 # distutils: language = c++
 
+from libc.stdio cimport fclose, FILE, fflush, fopen, fread, fseek, ftell, fwrite, SEEK_SET
 from libcpp.string cimport string
 
-cdef extern from "zlib.h":
+cdef extern from "<stdio.h>" nogil:
+    int fileno(FILE*)
+
+cdef extern from "zlib.h" nogil:
     ctypedef void* gzFile
     ctypedef size_t z_off_t
 
     int gzclose(gzFile fp)
     gzFile gzopen(char* path, char* mode)
+    gzFile gzdopen(int fd, char* mode)
     int gzread(gzFile fp, void* buf, unsigned int n)
     char* gzerror(gzFile fp, int* errnum)
 
 cdef size_t BUFF_SIZE = 16384
 cdef size_t STR_NPOS = <size_t> -1
 
-cdef class CompressedStream:
-    cdef void open(self, string file, string mode):
+
+cdef class IOStream:
+    cdef void close(self):
         pass
 
-    cdef void close(self):
+    cdef bint flush(self):
+        pass
+
+    cdef size_t tell(self):
         pass
 
     cdef int errnum(self):
@@ -44,18 +53,56 @@ cdef class CompressedStream:
     cdef string read(self, size_t size=BUFF_SIZE):
         pass
 
-cdef class GZipStream(CompressedStream):
+    cdef size_t write(self, char* data, size_t size):
+        pass
+
+
+cdef class FileStream(IOStream):
+    cdef FILE* fp
+
+    def __init__(self):
+        self.fp = NULL
+
+    cpdef void open(self, char* path, char* mode=b'rb'):
+        self.fp = fopen(path, mode)
+
+    cdef void close(self):
+        fclose(self.fp)
+
+    cdef bint flush(self):
+        fflush(self.fp)
+
+    cdef size_t tell(self):
+        return ftell(self.fp)
+
+    cdef void fseek(self, size_t pos):
+        fseek(self.fp, pos, SEEK_SET)
+
+    cdef string read(self, size_t size=BUFF_SIZE):
+        cdef string buf
+        buf.resize(size)
+        cdef size_t c = fread(&buf.front(), sizeof(char*), size, self.fp)
+        buf.resize(c)
+        return buf
+
+    cdef size_t write(self, char* data, size_t size):
+        return fwrite(data, sizeof(char*), size, self.fp)
+
+
+cdef class GZipStream(IOStream):
     cdef gzFile fp
 
-    def __init__(self, string filename, string mode):
+    def __init__(self):
         self.fp = NULL
-        self.open(filename, mode)
 
     def __dealloc__(self):
         self.close()
 
-    cdef void open(self, string file, string mode):
-        self.fp = gzopen(file.c_str(), mode.c_str())
+    cpdef void open(self, char* path, char* mode=b'rb'):
+        self.fp = gzopen(path, mode)
+
+    cpdef void open_from_stream(self, FileStream raw, char* mode=b'rb'):
+        self.fp = gzdopen(fileno(raw.fp), mode)
 
     cdef void close(self):
         if self.fp:
@@ -77,11 +124,12 @@ cdef class GZipStream(CompressedStream):
         cdef int l = gzread(self.fp, &buf.front(), size)
         return buf.substr(0, l)
 
+
 cdef class LineParser:
-    cdef CompressedStream stream
+    cdef IOStream stream
     cdef string buf
 
-    def __init__(self, CompressedStream stream):
+    def __init__(self, IOStream stream):
         self.stream = stream
         self.buf = string(b'')
 
