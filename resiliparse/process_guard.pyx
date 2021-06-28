5aad892ef1d5c1c751e2f2a01bf11feb14eed855
Janek Bevendorff <janek@jbev.net>
2021-06-28 23:34:09 +0200

    Move TimeGuard to class and implement progress() reporting

--- resiliparse/process_guard.pyx
+++ resiliparse/process_guard.pyx
@@ -16,7 +16,6 @@
 
 import inspect
 from threading import current_thread, Thread
-from typing import Callable
 
 from cpython cimport PyObject, PyThreadState_SetAsyncExc
 
@@ -32,14 +31,26 @@ cdef extern from "<pthread.h>" nogil:
     pthread_t pthread_self()
     int pthread_kill(pthread_t thread, int sig)
 
+    ctypedef struct pthread_mutex_t:
+        long sig
+        char* opaque
+    ctypedef struct pthread_mutexattr_t:
+        long sig
+        char * opaque
+    pthread_mutex_t PTHREAD_MUTEX_INITIALIZER
+    int pthread_mutex_init(pthread_mutex_t* mutex, const pthread_mutexattr_t* attr)
+    int pthread_mutex_destroy(pthread_mutex_t* mutex)
+    int pthread_mutex_lock(pthread_mutex_t* mutex)
+    int pthread_mutex_unlock(pthread_mutex_t* mutex)
+
 cdef extern from "<unistd.h>" nogil:
     int usleep(size_t usec)
 
 
-cdef struct _GuardStatus:
+cdef struct _GuardContext:
     size_t epoch_counter
     bint ended
-    void* lock
+    pthread_mutex_t lock
 
 
 cpdef enum InterruptType:
@@ -48,24 +59,124 @@ cpdef enum InterruptType:
     exception_then_signal
 
 
-class ResiliparseException(BaseException):
-    """Resiliparse base exception."""
+class ResiliparseGuardException(BaseException):
+    """Resiliparse guard base exception."""
 
 
-class ExecutionTimeout(ResiliparseException):
+class ExecutionTimeout(ResiliparseGuardException):
     """Execution timeout exception."""
 
 
-class MemoryLimitExceeded(ResiliparseException):
+class MemoryLimitExceeded(ResiliparseGuardException):
     """Memory limit exceeded exception."""
 
 
-def timer_guard(size_t timeout, size_t grace_period=15, InterruptType interrupt_type=exception_then_signal):
+cdef class _ResiliparseGuard:
+    cdef _GuardContext gctx
+
+    def __cinit__(self, *args, **kwargs):
+        self.gctx.epoch_counter = 0
+        self.gctx.ended = False
+        self.gctx.lock = PTHREAD_MUTEX_INITIALIZER
+
+    def __dealloc__(self):
+        self.finish()
+
+    cdef void finish(self):
+        if not self.gctx.ended:
+            self.gctx.ended = True
+            pthread_mutex_destroy(&self.gctx.lock)
+
+    def __call__(self, func):
+        def guard_wrapper(*args, **kwargs):
+            self.exec_before()
+            ret = func(*args, **kwargs)
+            self.exec_after()
+            self.finish()
+            return ret
+
+        # Retain `self`, but do not bind via `__get__()` or else `func` will belong to this class
+        guard_wrapper._self = self
+        return guard_wrapper
+
+    cdef void exec_before(self):
+        pass
+
+    cdef void exec_after(self):
+        pass
+
+
+# noinspection PyAttributeOutsideInit
+cdef class _TimeGuard(_ResiliparseGuard):
+    cdef size_t timeout
+    cdef size_t grace_period
+    cdef InterruptType interrupt_type
+
+    def __cinit__(self, size_t timeout, size_t grace_period, InterruptType interrupt_type):
+        self.timeout = timeout
+        self.grace_period = grace_period
+        self.interrupt_type = interrupt_type
+
+    cdef void exec_before(self):
+        # Save pthread and Python thread IDs (they should be the same, but don't take chances)
+        cdef unsigned long main_thread_ident = current_thread().ident
+        cdef pthread_t main_thread_id = pthread_self()
+
+        def _thread_exec():
+            cdef size_t sec_ctr = 0
+            cdef size_t last_epoch = 0
+
+            with nogil:
+                while True:
+                    if self.gctx.ended:
+                        break
+
+                    usleep(500 * 1000)
+
+                    pthread_mutex_lock(&self.gctx.lock)
+                    if self.gctx.epoch_counter > last_epoch:
+                        sec_ctr = 0
+                        last_epoch = self.gctx.epoch_counter
+                    else:
+                        sec_ctr += 1
+                    pthread_mutex_unlock(&self.gctx.lock)
+
+                    # Exceeded, but within grace period
+                    if sec_ctr == self.timeout * 2:
+                        if self.interrupt_type == exception or self.interrupt_type == exception_then_signal:
+                            with gil:
+                                PyThreadState_SetAsyncExc(main_thread_ident, <PyObject*>ExecutionTimeout)
+                        elif self.interrupt_type == signal:
+                            pthread_kill(main_thread_id, SIGINT)
+
+                    # Grace period exceeded
+                    elif sec_ctr == (self.timeout + self.grace_period) * 2:
+                        if self.interrupt_type == signal:
+                            pthread_kill(main_thread_id, SIGTERM)
+                        elif self.interrupt_type == exception_then_signal:
+                            pthread_kill(main_thread_id, SIGINT)
+                        elif self.interrupt_type == exception:
+                            with gil:
+                                PyThreadState_SetAsyncExc(main_thread_ident, <PyObject*>ExecutionTimeout)
+
+                    # If process still hasn't reacted, send SIGTERM and then exit
+                    elif sec_ctr >= (self.timeout + self.grace_period * 2) * 2:
+                        if self.interrupt_type != exception:
+                            pthread_kill(main_thread_id, SIGTERM)
+                        break
+
+        cdef guard_thread = Thread(target=_thread_exec)
+        guard_thread.setDaemon(True)
+        guard_thread.start()
+
+
+def time_guard(size_t timeout, size_t grace_period=15, InterruptType interrupt_type=exception_then_signal):
     """
     Decorator for guarding execution time of a function.
 
     If a function runs longer than the pre-defined timeout, the guard will send an
-    interrupt to the running function context.
+    interrupt to the running function context. To signal progress to the guard and reset
+    the timeout, call :func:`refresh()` from the guarded context.
 
     There are two interrupt mechanisms: throwing an asynchronous exception and sending
     a UNIX signal. The exception mechanism is the most gentle method of the two, but
@@ -91,89 +202,34 @@ def timer_guard(size_t timeout, size_t grace_period=15, InterruptType interrupt_
     :param interrupt_type: type of interrupt (default: `InterruptType.exception_then_signal`)
 
     """
-    def decorator(func: Callable):
-        cdef _GuardStatus gs
-        gs.epoch_counter = 0
-        gs.ended = False
-        # pthread_mutex_init(&gs.lock, NULL)
-
-        def wrapper(*args, **kwargs):
-            _init_timer_guard(
-                func=func,
-                guard_status=&gs,
-                timeout=timeout,
-                grace_period=grace_period,
-                interrupt_type=interrupt_type)
-            print(func, 'f')
-            func(*args, **kwargs)
-            gs.ended = True
-
-        wrapper._rp_gs = <unsigned long>&gs
-        return wrapper
-
-    return decorator
+    return _TimeGuard.__new__(_TimeGuard, timeout, grace_period, interrupt_type)
 
 
-def progress(caller):
-    cdef _GuardStatus* gs = <_GuardStatus*>caller._rp_gs
-    # pthread_mutex_lock(&gs.lock)
-    gs.epoch_counter += 1
-    # pthread_mutex_unlock(&gs.lock)
+def progress(caller=None):
+    """
+    Increment guard epoch counter to indicate progress and reset the guard timeout.
 
+    If `caller` ist `None`, the last valid guard context from the global namespace on
+    the call stack will be used. If the guard context does not live in the module's
+    global namespace, this auto-detection will fail and the caller has to be supplied
+    explicitly.
 
-cdef void _init_timer_guard(func, _GuardStatus* guard_status,
-                            size_t timeout, size_t grace_period, InterruptType interrupt_type):
+    If `caller` ist not a valid guard context, the progress report will fail and a
+    :class:`RuntimeError` will be raised.
 
-    # Save pthread and Python thread IDs (they should be the same, but don't take chances)
-    cdef unsigned long main_thead_ident = current_thread().ident
-    cdef pthread_t main_thread_id = pthread_self()
+    :param caller: calling context (if None, last context from stack will be used)
+    """
+    if caller is None:
+        for i in range(len(inspect.stack())):
+            frame_info = inspect.stack()[i]
+            caller = frame_info[0].f_globals.get(frame_info[3])
+            if isinstance(getattr(caller, '_self', None), _ResiliparseGuard):
+                break
 
-    class TimerGuardThread(Thread):
-        def run(self):
-            cdef size_t i = 0
-            cdef size_t last_epoch = 0
+    if not isinstance(getattr(caller, '_self', None), _ResiliparseGuard):
+        raise RuntimeError('No initialized guard context.')
 
-            with nogil:
-                while True:
-                    if guard_status.ended:
-                        break
-
-                    usleep(500 * 1000)
-                    with gil:
-                        print(guard_status.epoch_counter)
-
-                    # pthread_mutex_lock(&guard_status.lock)
-                    if guard_status.epoch_counter > last_epoch:
-                        i = 0
-                        last_epoch = guard_status.epoch_counter
-                    else:
-                        i += 1
-                    # pthread_mutex_unlock(&guard_status.lock)
-
-                    # Exceeded, but within grace period
-                    if i == timeout * 2:
-                        if interrupt_type == exception or interrupt_type == exception_then_signal:
-                            with gil:
-                                PyThreadState_SetAsyncExc(main_thead_ident, <PyObject*>ExecutionTimeout)
-                        elif interrupt_type == signal:
-                            pthread_kill(main_thread_id, SIGINT)
-
-                    # Grace period exceeded
-                    elif i == (timeout + grace_period) * 2:
-                        if interrupt_type == signal:
-                            pthread_kill(main_thread_id, SIGTERM)
-                        elif interrupt_type == exception_then_signal:
-                            pthread_kill(main_thread_id, SIGINT)
-                        elif interrupt_type == exception:
-                            with gil:
-                                PyThreadState_SetAsyncExc(main_thead_ident, <PyObject*>ExecutionTimeout)
-
-                    # If process still hasn't reacted, send SIGTERM and then exit
-                    elif i >= (timeout + grace_period * 3) * 2:
-                        if interrupt_type != exception:
-                            pthread_kill(main_thread_id, SIGTERM)
-                        break
-
-    cdef guard_thread = TimerGuardThread()
-    guard_thread.setDaemon(True)
-    guard_thread.start()
+    cdef _GuardContext* gctx = &(<_ResiliparseGuard>caller._self).gctx
+    pthread_mutex_lock(&gctx.lock)
+    gctx.epoch_counter += 1
+    pthread_mutex_unlock(&gctx.lock)
