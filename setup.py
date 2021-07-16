6e107263b0895de66cfbe233cae4cce7c54fa970
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-07-16 17:05:32 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, Extension
 import warnings
 import sys
 
-VERSION = '0.3.10'
+VERSION = '0.3.11'
 THIS_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 USE_CYTHON = True
 try:
