a108f819abaaeb320f661a812090a20828c8943a
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-08-30 18:06:10 +0200

    Add basic HTML parsing logic

--- resiliparse/parse/html.pyx
+++ resiliparse/parse/html.pyx
@@ -17,11 +17,32 @@
 from libcpp.string cimport string
 from resiliparse_inc.lexbor cimport *
 
+from resiliparse.parse.encoding cimport bytes_to_str
+
+
+cdef class Node:
+    cdef str encoding
+    cdef lxb_dom_element_t* element
+
+    def __cinit__(self, str encoding='utf-8'):
+        self.encoding = encoding
+        self.element = NULL
+
+    @property
+    def text(self):
+        if self.element == NULL:
+            return None
+        cdef size_t text_len = 0
+        cdef lxb_char_t* text = lxb_dom_node_text_content(&self.element.node, &text_len)
+        return bytes_to_str(text[:text_len], self.encoding)
+
 
 cdef class HTMLTree:
     cdef lxb_html_document_t* document
+    cdef str encoding
 
-    def __cinit__(self):
+    def __cinit__(self, str encoding='utf-8'):
+        self.encoding = encoding
         self.document = lxb_html_document_create()
         if self.document == NULL:
             raise RuntimeError('Failed to allocate HTML document')
@@ -30,5 +51,36 @@ cdef class HTMLTree:
         if self.document != NULL:
             lxb_html_document_destroy(self.document)
 
-    cpdef parse(self, bytes document):
-        pass
+    cpdef parse(self, str document):
+        cdef string document_bytes = document.encode('utf-8')
+        self.parse_from_bytes(document_bytes)
+
+    cpdef parse_from_bytes(self, const string& document):
+        cdef lxb_status_t status = lxb_html_document_parse(self.document,
+                                                           <const lxb_char_t*>document.data(), document.size())
+        if status != LXB_STATUS_OK:
+            raise ValueError('Failed to parse HTML document')
+
+    @property
+    def head(self):
+        if self.document == NULL:
+            return None
+
+        cdef lxb_html_head_element_t* head = lxb_html_document_head_element(self.document)
+        if head == NULL:
+            return None
+        cdef Node node = Node.__new__(Node, self.encoding)
+        node.element = <lxb_dom_element_t*>head
+        return node
+
+    @property
+    def body(self):
+        if self.document == NULL:
+            return None
+
+        cdef lxb_html_body_element_t* body = lxb_html_document_body_element(self.document)
+        if body == NULL:
+            return None
+        cdef Node node = Node.__new__(Node, self.encoding)
+        node.element = <lxb_dom_element_t*>body
+        return node
