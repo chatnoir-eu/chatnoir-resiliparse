5fa384cf024dbc776b69e0d65243c8900f894c9b
Janek Bevendorff <janek@jbev.net>
2021-09-04 20:58:12 +0200

    Move HTML benchmarking tool to generic resiliparse.parse CLI module

--- resiliparse/parse/cli.py
+++ resiliparse/parse/cli.py
@@ -1,4 +1,21 @@
+# Copyright 2021 Janek Bevendorff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import codecs
+import json
 import time
+import urllib.request
 
 import click
 from tqdm import tqdm
@@ -8,9 +25,45 @@ from resiliparse.parse.encoding import detect_encoding, bytes_to_str
 from resiliparse.parse.html import HTMLTree
 
 
-@click.command()
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+def main():
+    """Resiliparse Parsing module CLI."""
+    pass
+
+
+@main.command(short_help='Download WHATWG encoding mapping.')
+def download_whatwg_mapping():
+    """
+    Download the current WHATWG encoding mapping, parse and transform it, and
+    then print it as a copyable Python dict.
+    """
+
+    encodings = json.load(urllib.request.urlopen('https://encoding.spec.whatwg.org/encodings.json'))
+
+    enc_mapped = {}
+    for enc in encodings:
+        for enc2 in enc['encodings']:
+            n = enc2['name'].lower()
+            try:
+                if n == 'iso-8859-8-i':
+                    n = 'iso-8859-8'
+                    enc_mapped[n.lower()] = n
+                elif n == 'windows-874':
+                    n = 'iso-8859-11'
+                n = codecs.lookup(n).name
+                enc_mapped[n] = n
+                for l in enc2['labels']:
+                    enc_mapped[l] = n
+            except LookupError:
+                click.echo(f'skipped {enc2["name"]}', err=True)
+    enc_mapped = dict((k, enc_mapped[k]) for k in sorted(enc_mapped))
+
+    click.echo(enc_mapped)
+
+
+@main.command(short_help='Benchmark Resiliparse HTML parser.')
 @click.argument('warc_file', type=click.Path(dir_okay=False, exists=True))
-def benchmark(warc_file):
+def benchmark_html(warc_file):
     """
     Benchmark Resiliparse HTML parsing by extracting the titles from all HTML pages in a WARC file.
 
@@ -72,4 +125,4 @@ def benchmark(warc_file):
 
 
 if __name__ == '__main__':
-    benchmark()
+    main()
