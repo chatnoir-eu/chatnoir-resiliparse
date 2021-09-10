1cb64318f1b9f41be841f2d7d6a93756d51e2299
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-09-10 09:55:55 +0200

    Add HTTP tests

---
+++ tests/resiliparse/parse/test_http.py
@@ -0,0 +1,24 @@
+from io import BytesIO
+from resiliparse.parse.http import *
+
+
+chunked_http = b'''c\r\n\
+Resiliparse \r\n\
+6\r\n\
+is an \r\n\
+8\r\n\
+awesome \r\n\
+5\r\n\
+tool.\r\n\
+0\r\n\
+\r\n'''
+
+
+def test_chunked_http():
+    reader = BytesIO(chunked_http)
+    decoded = b''
+    while chunk := read_http_chunk(reader):
+        decoded += chunk
+
+    assert decoded == b'Resiliparse is an awesome tool.'
+    assert b''.join(iterate_http_chunks(BytesIO(chunked_http))) == b'Resiliparse is an awesome tool.'
