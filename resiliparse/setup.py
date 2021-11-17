e4370437bc871c2409514c507fcdab82e45a32d0
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-11-17 20:12:46 +0100

    Add -vc140-mt to boost lib name on MSVC

--- resiliparse/setup.py
+++ resiliparse/setup.py
@@ -98,9 +98,11 @@ fast_warc_src = os.path.abspath(os.path.join(ROOT_DIRECTORY, '..', 'fastwarc'))
 if os.path.isdir(fast_warc_src):
     sys.path.insert(0, fast_warc_src)
 
+boost_suffix = '-vc140-mt' if CXX == 'msvc' else ''
 resiliparse_extensions = [
     Extension('resiliparse.extract.html2text',
-              sources=[f'resiliparse/extract/html2text.{ext}'], libraries=['lexbor', 'boost_regex'], **cpp_args),
+              sources=[f'resiliparse/extract/html2text.{ext}'],
+              libraries=['lexbor', 'boost_regex' + boost_suffix], **cpp_args),
     Extension('resiliparse.parse.encoding',
               sources=[f'resiliparse/parse/encoding.{ext}'], libraries=['uchardet', 'lexbor'], **cpp_args),
     Extension('resiliparse.parse.html',
