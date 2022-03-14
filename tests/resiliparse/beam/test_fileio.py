d6bed7ceaaf2cfd00c38b490c0d581ca3ae695c0
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2022-03-14 15:14:05 +0100

    Add beam.fileio tests

---
+++ tests/resiliparse/beam/test_fileio.py
@@ -0,0 +1,25 @@
+import os
+
+import apache_beam as beam
+from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import assert_that, equal_to
+from resiliparse.beam import fileio
+
+
+DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+
+
+def test_matchfiles():
+    # Match without shuffle
+    with TestPipeline() as pipeline:
+        count = (pipeline
+                 | fileio.MatchFiles(os.path.join(DATA_DIR, 'warcfile*'), shuffle=False)
+                 | beam.combiners.Count.Globally())
+        assert_that(count, equal_to([3]))
+
+    # Match with shuffle
+    with TestPipeline() as pipeline:
+        count = (pipeline
+                 | fileio.MatchFiles(os.path.join(DATA_DIR, 'warcfile*'), shuffle=True)
+                 | beam.combiners.Count.Globally())
+        assert_that(count, equal_to([3]))
