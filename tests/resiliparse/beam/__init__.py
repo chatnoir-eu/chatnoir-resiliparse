b725dcd0dd48b6446d70b51b59726e8ecadb802f
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2022-03-15 14:28:09 +0100

    Ensure Python version is compatible before running Beam tests

--- tests/resiliparse/beam/__init__.py
+++ tests/resiliparse/beam/__init__.py
@@ -1,6 +1,13 @@
 import pytest
+import sys
 
 try:
     import apache_beam as beam
 except ModuleNotFoundError:
-    pytest.skip("Beam dependency not installed, skipping tests.", allow_module_level=True)
+    pytest.skip("Apache Beam dependency not installed.", allow_module_level=True)
+
+if beam.__version__ < '2.37.0' and sys.version_info.minor >= 9:
+    pytest.skip('Apache Beam < 2.37.0 not yet supported on Python 3.9 or higher.', allow_module_level=True)
+
+if sys.version_info.minor > 9:
+    pytest.skip('Apache Beam not yet supported on Python 3.10 or higher.', allow_module_level=True)
