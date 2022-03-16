efe2c00697bb6a055d43dc9e975ac80924611679
Janek Bevendorff <janek@jbev.net>
2022-03-16 22:09:29 +0100

    Add PATH components to DLL search path for running tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,11 @@ sys.path.extend([
     os.path.join(src_dir, 'resiliparse'),
     os.path.join(src_dir, 'fastwarc')
 ])
+
+if sys.platform == 'win32' and sys.version_info.minor >= 8:
+    # Restore pre-3.8 DLL loading behaviour by adding PATH components to the DLL search paths.
+    # We need this to load linked DLLs from non-standard places (such as Vcpkg), otherwise those
+    # DLLs would have to be copied all over the place for running tests.
+    for p in os.environ['PATH'].split(';'):
+        if p:
+            os.add_dll_directory(p)
