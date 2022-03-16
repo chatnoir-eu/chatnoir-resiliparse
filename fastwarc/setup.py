cef43164e7b869b8d49455afb54fc1d38e7f6ad1
Janek Bevendorff <janek@jbev.net>
2022-03-16 23:29:01 +0100

    Bump version number

--- fastwarc/setup.py
+++ fastwarc/setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup, Extension
 
 # 1. BOILERPLATE -------------------------------------------------------
 
-VERSION = '0.11.1'
+VERSION = '0.11.2'
 ROOT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 CXX = distutils.ccompiler.get_default_compiler()
 
