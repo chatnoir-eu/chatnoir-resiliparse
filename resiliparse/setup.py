e465db889819719fbf89ae3cde893bc87b6216a6
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2023-03-07 19:13:46 +0100

    Add Rust test module

--- resiliparse/setup.py
+++ resiliparse/setup.py
@@ -22,6 +22,7 @@ from Cython.Build import cythonize
 from Cython.Distutils.build_ext import new_build_ext as build_ext
 import distutils.ccompiler
 from setuptools import Extension, setup
+from setuptools_rust import Binding, RustExtension
 
 TRACE = bool(int(os.getenv('TRACE', 0)))
 DEBUG = bool(int(os.getenv('DEBUG', 0))) or TRACE
@@ -116,6 +117,7 @@ if fastwarc_headers:
 
 setup(
     ext_modules=get_ext_modules(),
+    rust_extensions=[RustExtension('resiliparse.rust_test', binding=Binding.PyO3)],
     cmdclass=dict(build_ext=build_ext),
     exclude_package_data={
         '': [] if 'sdist' in sys.argv else ['*.pxd', '*.pxi', '*.pyx', '*.h', '*.cpp']
