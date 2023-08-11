92270ffac1552199cc4393781109bd33a29d7d32
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2023-08-11 10:17:39 +0200

    Use custom vcpkg triplets

---
+++ .vcpkg/triplets/arm64-osx.cmake
@@ -0,0 +1,12 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
+
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_C_FLAGS "")
+set(VCPKG_CXX_FLAGS "-std=c++17")
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+if(PORT MATCHES "abseil")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
