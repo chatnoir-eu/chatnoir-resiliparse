8afa4bc6aa802c33ee27d1bbeea778aa040d45a0
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2024-11-27 18:12:37 +0100

    Add helper script for downloading and compiling 3rd-party libs

--- compile-third-party-libs.sh
+++ compile-third-party-libs.sh
@@ -1,30 +1,29 @@
-ARG TARGET_ARCH
-FROM quay.io/pypa/manylinux_2_28_${TARGET_ARCH}:latest
+#!/usr/bin/env bash
 
-RUN set -x \
-    && yum install -y \
-          gcc-toolset-12-libasan-devel \
-          lz4-devel \
-          zlib-devel
+# Helper script for downloading and compiling third-party libs to ./lib-third-party.
+# Set CPATH to lib-third-party/include and (LD_)LIBRARY_PATH to lib-third-party/lib
+# to use the downloading these libs instead of the version installed on your system.
 
-RUN set -x \
-    && curl -Lf https://gitlab.freedesktop.org/uchardet/uchardet/-/archive/v0.0.8/uchardet-v0.0.8.tar.gz | tar xz \
+set -xe
+
+mkdir -p lib-third-party && cd lib-third-party
+
+curl -Lf https://gitlab.freedesktop.org/uchardet/uchardet/-/archive/v0.0.8/uchardet-v0.0.8.tar.gz | tar xz \
     && (cd uchardet-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
+            -DCMAKE_INSTALL_PREFIX=$(pwd)/.. \
             -DCMAKE_CXX_STANDARD=17 \
             -DLEXBOR_BUILD_STATIC=ON \
             -B build \
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf uchardet*
 
-RUN set -x \
-    && curl -Lf https://github.com/lexbor/lexbor/archive/refs/tags/v2.3.0.tar.gz | tar xz \
+curl -Lf https://github.com/lexbor/lexbor/archive/refs/tags/v2.3.0.tar.gz | tar xz \
     && (cd lexbor-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
+            -DCMAKE_INSTALL_PREFIX=$(pwd)/.. \
             -DLEXBOR_BUILD_SHARED=ON \
             -DLEXBOR_BUILD_STATIC=OFF \
             -DLEXBOR_OPTIMIZATION_LEVEL=-O3 \
@@ -32,12 +31,11 @@ RUN set -x \
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf lexbor*
 
-RUN set -x \
-    && curl -Lf https://github.com/abseil/abseil-cpp/releases/download/20240116.1/abseil-cpp-20240116.1.tar.gz | tar xz \
+curl -Lf https://github.com/abseil/abseil-cpp/releases/download/20240116.1/abseil-cpp-20240116.1.tar.gz | tar xz \
     && (cd abseil-cpp-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
+            -DCMAKE_INSTALL_PREFIX=$(pwd)/.. \
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
             -DCMAKE_CXX_STANDARD=17 \
             -DBUILD_SHARED_LIBS=OFF \
@@ -45,12 +43,11 @@ RUN set -x \
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf abseil*
 
-RUN set -x \
-    && curl -Lf https://github.com/google/re2/releases/download/2024-04-01/re2-2024-04-01.tar.gz | tar xz \
+curl -Lf https://github.com/google/re2/releases/download/2024-04-01/re2-2024-04-01.tar.gz | tar xz \
     && (cd re2-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
+            -DCMAKE_INSTALL_PREFIX=$(pwd)/.. \
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
             -DCMAKE_CXX_STANDARD=17 \
             -DBUILD_SHARED_LIBS=ON \
@@ -58,3 +55,5 @@ RUN set -x \
             -B build \
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf re2*
+
+cd ..
