8407e57238cd6a123fe25b3efc04988ba600e423
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2024-12-03 16:29:34 +0100

    Update dependencies

--- Dockerfile
+++ Dockerfile
@@ -7,54 +7,6 @@ RUN set -x \
           lz4-devel \
           zlib-devel
 
+COPY compile-third-party-libs.sh /usr/bin/compile-third-party-libs
 RUN set -x \
-    && curl -Lf https://gitlab.freedesktop.org/uchardet/uchardet/-/archive/v0.0.8/uchardet-v0.0.8.tar.gz | tar xz \
-    && (cd uchardet-* && mkdir build \
-        && cmake \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
-            -DCMAKE_CXX_STANDARD=17 \
-            -DLEXBOR_BUILD_STATIC=ON \
-            -B build \
-        && cmake --build build -j$(nproc) --target install) \
-    && rm -rf uchardet*
-
-RUN set -x \
-    && curl -Lf https://github.com/lexbor/lexbor/archive/refs/tags/v2.3.0.tar.gz | tar xz \
-    && (cd lexbor-* && mkdir build \
-        && cmake \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
-            -DLEXBOR_BUILD_SHARED=ON \
-            -DLEXBOR_BUILD_STATIC=OFF \
-            -DLEXBOR_OPTIMIZATION_LEVEL=-O3 \
-            -B build \
-        && cmake --build build -j$(nproc) --target install) \
-    && rm -rf lexbor*
-
-RUN set -x \
-    && curl -Lf https://github.com/abseil/abseil-cpp/releases/download/20240116.1/abseil-cpp-20240116.1.tar.gz | tar xz \
-    && (cd abseil-cpp-* && mkdir build \
-        && cmake \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
-            -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-            -DCMAKE_CXX_STANDARD=17 \
-            -DBUILD_SHARED_LIBS=OFF \
-            -B build \
-        && cmake --build build -j$(nproc) --target install) \
-    && rm -rf abseil*
-
-RUN set -x \
-    && curl -Lf https://github.com/google/re2/releases/download/2024-04-01/re2-2024-04-01.tar.gz | tar xz \
-    && (cd re2-* && mkdir build \
-        && cmake \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX=/usr \
-            -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-            -DCMAKE_CXX_STANDARD=17 \
-            -DBUILD_SHARED_LIBS=ON \
-            -DRE2_BUILD_TESTING=OFF \
-            -B build \
-        && cmake --build build -j$(nproc) --target install) \
-    && rm -rf re2*
+  && /usr/bin/compile-third-party-libs /usr
--- compile-third-party-libs.sh
+++ compile-third-party-libs.sh
@@ -6,30 +6,39 @@
 
 set -xe
 
-mkdir -p lib-third-party && cd lib-third-party
-export BASE_DIR="$(pwd)"
-export CPATH="${BASE_DIR}/include:$CPATH"
-export LIBRARY_PATH="${BASE_DIR}/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="${BASE_DIR}/lib:$LD_LIBRARY_PATH"
+if [ -n "$1" ]; then
+    cd "$1"
+else
+    mkdir -p lib-third-party && cd lib-third-party
+fi
 
-curl -Lf https://gitlab.freedesktop.org/uchardet/uchardet/-/archive/v0.0.8/uchardet-v0.0.8.tar.gz | tar xz \
+UCHARDET_VERSION=0.0.8
+LEXBOR_VERSION=2.4.0
+ABSEILL_VERSION=20240722.0
+RE2_VERSION=2024-07-02
+
+export BASE_DIR="$(pwd)"
+
+curl -Lf https://gitlab.freedesktop.org/uchardet/uchardet/-/archive/v${UCHARDET_VERSION}/uchardet-v${UCHARDET_VERSION}.tar.gz | tar xz \
     && (cd uchardet-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_INSTALL_PREFIX="${BASE_DIR}" \
             -DCMAKE_MODULE_PATH="${BASE_DIR}/lib/cmake" \
+            -DCMAKE_CXX_FLAGS="-I${BASE_DIR}/include -L${BASE_DIR}/lib" \
             -DCMAKE_CXX_STANDARD=17 \
             -DLEXBOR_BUILD_STATIC=ON \
             -B build \
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf uchardet*
 
-curl -Lf https://github.com/lexbor/lexbor/archive/refs/tags/v2.3.0.tar.gz | tar xz \
+curl -Lf https://github.com/lexbor/lexbor/archive/refs/tags/v${LEXBOR_VERSION}.tar.gz | tar xz \
     && (cd lexbor-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_INSTALL_PREFIX="${BASE_DIR}" \
             -DCMAKE_MODULE_PATH="${BASE_DIR}/lib/cmake" \
+            -DCMAKE_CXX_FLAGS="-I${BASE_DIR}/include -L${BASE_DIR}/lib" \
             -DLEXBOR_BUILD_SHARED=ON \
             -DLEXBOR_BUILD_STATIC=OFF \
             -DLEXBOR_OPTIMIZATION_LEVEL=-O3 \
@@ -37,12 +46,13 @@ curl -Lf https://github.com/lexbor/lexbor/archive/refs/tags/v2.3.0.tar.gz | tar 
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf lexbor*
 
-curl -Lf https://github.com/abseil/abseil-cpp/releases/download/20240116.1/abseil-cpp-20240116.1.tar.gz | tar xz \
+curl -Lf https://github.com/abseil/abseil-cpp/releases/download/${ABSEILL_VERSION}/abseil-cpp-${ABSEILL_VERSION}.tar.gz | tar xz \
     && (cd abseil-cpp-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_INSTALL_PREFIX="${BASE_DIR}" \
             -DCMAKE_MODULE_PATH="${BASE_DIR}/lib/cmake" \
+            -DCMAKE_CXX_FLAGS="-I${BASE_DIR}/include -L${BASE_DIR}/lib" \
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
             -DCMAKE_CXX_STANDARD=17 \
             -DBUILD_SHARED_LIBS=OFF \
@@ -50,12 +60,13 @@ curl -Lf https://github.com/abseil/abseil-cpp/releases/download/20240116.1/absei
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf abseil*
 
-curl -Lf https://github.com/google/re2/releases/download/2024-04-01/re2-2024-04-01.tar.gz | tar xz \
+curl -Lf https://github.com/google/re2/releases/download/${RE2_VERSION}/re2-${RE2_VERSION}.tar.gz | tar xz \
     && (cd re2-* && mkdir build \
         && cmake \
             -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_INSTALL_PREFIX="${BASE_DIR}" \
             -DCMAKE_MODULE_PATH="${BASE_DIR}/lib/cmake" \
+            -DCMAKE_CXX_FLAGS="-I${BASE_DIR}/include -L${BASE_DIR}/lib" \
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
             -DCMAKE_CXX_STANDARD=17 \
             -DBUILD_SHARED_LIBS=ON \
@@ -63,5 +74,3 @@ curl -Lf https://github.com/google/re2/releases/download/2024-04-01/re2-2024-04-
             -B build \
         && cmake --build build -j$(nproc) --target install) \
     && rm -rf re2*
-
-cd ..
